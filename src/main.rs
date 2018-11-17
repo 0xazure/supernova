f4838f95e503f73bb8ed51abf790f66bb1febcf4
0xazure <stuart.crust@gmail.com>
2018-11-17 17:18:14 -0500

    refactor: pass request token as a flag instead of an argument
    Improves the calling convention to make the data passed more explicit and help clarify usage. This also has the added benefit of improving discoverability as we can generate autocomplete help from the flag to guide the correct usage.
    
    The calling convention was:
    
    `supernova <username> [<auth-token>]`
    
    The calling convention is now:
    
    `supernova <username> [--token <token>]`
    
    Using clap also gives us help and version commands and display for free in addition to argument parsing.
    
    Closes #16.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+extern crate clap;
 extern crate supernova;
 
-use std::{env, process};
+use clap::{App, Arg, crate_name, crate_version};
+use std::process;
 use supernova::Config;
 
 fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    let config: Config = App::new(crate_name!())
+        .version(crate_version!())
+        .arg(
+            Arg::with_name("USERNAME")
+                .help("The user whose stars to collect")
+                .required(true),
+        ).arg(
+            Arg::with_name("TOKEN")
+                .short("t")
+                .long("token")
+                .help("Sets the authentication token for requests to GitHub")
+                .takes_value(true),
+        ).get_matches()
+        .into();
 
     if let Err(e) = supernova::collect_stars(config) {
         println!("Application error: {}", e);
